0b6eac2e093be0663dcefe4b8015ab3a7df0fd32
ghoulbel <bel.g@gmx.ch>
2024-02-07 15:40:35 +0100

    input and output path as parameters

--- fs.py
+++ fs.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import glob
 import xml.etree.ElementTree as ET
+import sys
 
 def split_xml(input_files, output_dir):
     for input_file in input_files:
@@ -19,8 +20,21 @@ def split_xml(input_files, output_dir):
         os.makedirs(archive_dir, exist_ok=True)
         shutil.move(input_file, os.path.join(archive_dir, os.path.basename(input_file)))
 
+# if __name__ == "__main__":
+#     input_files = glob.glob("data/IN/*.xml")
+#     output_dir = "data/OUT"
+#     os.makedirs(output_dir, exist_ok=True)
+#     split_xml(input_files, output_dir)
+
 if __name__ == "__main__":
-    input_files = glob.glob("data/IN/*.xml")
-    output_dir = "data/OUT"
-    os.makedirs(output_dir, exist_ok=True)
-    split_xml(input_files, output_dir)
+    if len(sys.argv) != 3:
+        print("Usage: python script_name.py input_path output_path")
+        sys.exit(1)
+
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    input_files = glob.glob(os.path.join(input_path, "*.xml"))
+
+    os.makedirs(output_path, exist_ok=True)
+    split_xml(input_files, output_path)
