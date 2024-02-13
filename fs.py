44550ff6071121d78b68d057c6e5fcc83d79280b
ghoulbel <bel@integon.ch>
2024-02-13 14:55:44 +0100

    finalised parameterised split and filesize

--- fs.py
+++ fs.py
@@ -29,11 +29,11 @@ def is_utf8(filename):
 def is_large_file(filename, size_limit):
     return os.path.getsize(filename) > size_limit
         
-def split_xml(input_files, output_dir):
+def split_xml(input_files, output_dir, max_file_size, split_file_size):
     for input_file in input_files:
         namespace, release = extract_namespace_and_release(input_file)
         version = release.replace('.', '_')
-        if release == "3.0" and is_utf8(input_file) and is_large_file(input_file, 100 * 1024 * 1024):
+        if release == "3.0" and is_utf8(input_file) and is_large_file(input_file, max_file_size):
             output_dir_archive = os.path.join(os.path.dirname(input_file), "archive")
             os.makedirs(output_dir_archive, exist_ok=True)
 
@@ -46,7 +46,6 @@ def split_xml(input_files, output_dir):
                 onix_message = ET.Element('ONIXmessage', {'xmlns': namespace, 'release': release})
                 onix_message.append(header)
                 current_file_size = len(ET.tostring(onix_message, encoding='utf-8'))
-                max_file_size = 100 * 1024 * 1024
                 i = 1
 
                 for event, elem in context:
@@ -56,7 +55,7 @@ def split_xml(input_files, output_dir):
                         current_file_size += len(elem_string)
 
                         # Check if the file size limit is exceeded
-                        if current_file_size >= max_file_size:
+                        if current_file_size >= split_file_size:
                             clean_onix_message(onix_message)
                             output_filename = f"Orig_{version}_{os.path.splitext(os.path.basename(input_file))[0]}_{i}.xml"
                             output_file = os.path.join(output_dir, output_filename)
@@ -83,19 +82,21 @@ def split_xml(input_files, output_dir):
             shutil.move(input_file, os.path.join(output_dir, output_filename))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script_name.py input_path output_path")
+    if len(sys.argv) != 5:
+        print("Usage: python script_name.py input_path output_path max_file_size split_file_size")
         sys.exit(1)
 
     input_path = sys.argv[1]
     output_path = sys.argv[2]
+    max_file_size = int(sys.argv[3])
+    split_file_size = int(sys.argv[4])
 
     os.makedirs(output_path, exist_ok=True)
 
     while True:
         input_files = glob.glob(os.path.join(input_path, "*.xml"))
         if input_files:
-            split_xml(input_files, output_path)
+            split_xml(input_files, output_path, max_file_size, split_file_size)
         time.sleep(15)
 
 
@@ -111,12 +112,14 @@ if __name__ == "__main__":
 # if __name__ == "__main__":
 #     input_files = glob.glob("data/IN/*.xml")
 #     output_dir = "data/OUT"
+#     max_file_size = 500 * 1024 * 1024
+#     split_file_size = 200 * 1024 * 1024
 #     os.makedirs(output_dir, exist_ok=True)
 #     split_xml(input_files, output_dir)
 
 
 
-#Debug
+#Debug --> python3 fs.py data/IN/ data/OUT/ $((500*1024*1024)) $((200*1024*1024))
 # if __name__ == "__main__":
 #     if len(sys.argv) != 3:
 #         print("Usage: python script_name.py input_path output_path")
